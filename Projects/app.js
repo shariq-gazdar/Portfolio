5c0c828831d41a7f4989925f1b880d8bb1c3d91c
Shariq583 <gazdarshariq0@gmail.com>
2024-07-16 00:41:21 +0500

    calculator add!

--- Projects/app.js
+++ Projects/app.js
@@ -1,16 +1,36 @@
 let screen = document.querySelector("#calcScreen");
 // All Buttons
-let oneBtn = document.querySelector("#oneBtn");
-let twoBtn = document.querySelector("#twoBtn");
-let threeBtn = document.querySelector("#threeBtn");
-let fourBtn = document.querySelector("#fourBtn");
-let fiveBtn = document.querySelector("#fiveBtn");
-let sixBtn = document.querySelector("#sixBtn");
-let sevenBtn = document.querySelector("#sevenBtn");
-let eightBtn = document.querySelector("#eightBtn");
-let nineBtn = document.querySelector("#nineBtn");
+let numBtn = document.querySelectorAll(".calcBtn");
 let plusBtn = document.querySelector("#plusBtn");
 let minusBtn = document.querySelector("#minusBtn");
 let multiplyBtn = document.querySelector("#multiplyBtn");
 let divideBtn = document.querySelector("#divideBtn");
 let ceBtn = document.querySelector("#ceBtn");
+let equalBtn = document.querySelector("#equalBtn");
+let num = [];
+let num2 = ``;
+let num3 = ``;
+let sign = ``;
+// num Buttons
+numBtn.forEach((i) => {
+  i.addEventListener("click", (e) => {
+    num.push(e.target.value);
+    screen.innerText = num.join("");
+    screen.scrollLeft = screen.scrollWidth;
+  });
+});
+equalBtn.addEventListener("click", () => {
+  calcFunction();
+});
+calcFunction = () => {
+  let numCalc = num.join("");
+  let answer = eval(numCalc);
+  screen.innerText = answer;
+};
+
+// Clear Button
+ceBtn.addEventListener("click", () => {
+  num1 = 0;
+  screen.innerText = num1;
+  location.reload();
+});
