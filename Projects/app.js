9d7af5874c78653219746a537218740cced15b7e
Shariq583 <gazdarshariq0@gmail.com>
2024-07-15 15:09:31 +0500

    btns add!

---
+++ Projects/app.js
@@ -0,0 +1,16 @@
+let screen = document.querySelector("#calcScreen");
+// All Buttons
+let oneBtn = document.querySelector("#oneBtn");
+let twoBtn = document.querySelector("#twoBtn");
+let threeBtn = document.querySelector("#threeBtn");
+let fourBtn = document.querySelector("#fourBtn");
+let fiveBtn = document.querySelector("#fiveBtn");
+let sixBtn = document.querySelector("#sixBtn");
+let sevenBtn = document.querySelector("#sevenBtn");
+let eightBtn = document.querySelector("#eightBtn");
+let nineBtn = document.querySelector("#nineBtn");
+let plusBtn = document.querySelector("#plusBtn");
+let minusBtn = document.querySelector("#minusBtn");
+let multiplyBtn = document.querySelector("#multiplyBtn");
+let divideBtn = document.querySelector("#divideBtn");
+let ceBtn = document.querySelector("#ceBtn");
