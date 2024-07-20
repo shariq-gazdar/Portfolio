23b35e3669bec58926f362a986cfbc5ea5cf5629
Shariq583 <gazdarshariq0@gmail.com>
2024-07-20 17:19:01 +0500

    weather app!

--- Projects/Weather App/app.js
+++ Projects/Weather App/app.js
@@ -1,71 +1,73 @@
 let triggerBtn = document.querySelector("#triggerBtn");
 
-triggerBtn.addEventListener("click", async () => {
-  let apiKey = `662a084d5f26412ca1e153450241807`;
-  let weatherSection = document.querySelector("#weatherSection");
-  weatherSection.classList.remove("hidden");
-  let cityName = document.querySelector("#cityName").value;
-  let baseUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${cityName}&aqi=no`;
+document.addEventListener("DOMContentLoaded", () => {
+  triggerBtn.addEventListener("click", async () => {
+    let apiKey = `662a084d5f26412ca1e153450241807`;
+    let weatherSection = document.querySelector("#weatherSection");
+    weatherSection.classList.remove("hidden");
+    let cityName = document.querySelector("#cityName").value;
+    let baseUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${cityName}&aqi=no`;
 
-  async function weatherUpdate(x, y) {
-    if (cityName !== "") {
-      console.log(cityName);
+    async function weatherUpdate(x, y) {
+      if (cityName !== "") {
+        console.log(cityName);
 
-      try {
-        let response = await fetch(baseUrl);
-        if (!response.ok) {
-          throw new Error(
-            `Network response was not ok: ${response.statusText}`
-          );
+        try {
+          let response = await fetch(baseUrl);
+          if (!response.ok) {
+            throw new Error(
+              `Network response was not ok: ${response.statusText}`
+            );
+          }
+          let name = document.querySelector("#cityNameDisplay");
+          let temp = document.querySelector("#temperatureDisplay");
+          let humidity = document.querySelector("#humidityDisplay");
+          let weatherIcon = document.querySelector("#weatherIcon");
+          let windDisplay = document.querySelector("#windDisplay");
+          let resultJson = await response.json();
+          console.log(resultJson);
+          name.innerText = resultJson.location.name;
+          temp.innerText = `${resultJson.current.temp_c}°C`;
+          humidity.innerText = `${resultJson.current.humidity}%`;
+          windDisplay.innerText = `${resultJson.current.wind_kph}kph`;
+          weatherIcon.src = `https:${resultJson.current.condition.icon}`;
+        } catch (error) {
+          console.error("Error:", error);
         }
-        let name = document.querySelector("#cityNameDisplay");
-        let temp = document.querySelector("#temperatureDisplay");
-        let humidity = document.querySelector("#humidityDisplay");
-        let weatherIcon = document.querySelector("#weatherIcon");
-        let windDisplay = document.querySelector("#windDisplay");
-        let resultJson = await response.json();
-        console.log(resultJson);
-        name.innerText = resultJson.location.name;
-        temp.innerText = `${resultJson.current.temp_c}°C`;
-        humidity.innerText = `${resultJson.current.humidity}%`;
-        windDisplay.innerText = `${resultJson.current.wind_kph}kph`;
-        weatherIcon.src = `https:${resultJson.current.condition.icon}`;
-      } catch (error) {
-        console.error("Error:", error);
-      }
-    } else {
-      let apiKey = `662a084d5f26412ca1e153450241807`;
-      let weatherSection = document.querySelector("#weatherSection");
-      weatherSection.classList.remove("hidden");
-      let cityName = document.querySelector("#cityName").value;
-      let baseUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${x},${y}&aqi=no`;
+      } else {
+        let apiKey = `662a084d5f26412ca1e153450241807`;
+        let weatherSection = document.querySelector("#weatherSection");
+        weatherSection.classList.remove("hidden");
+        let cityName = document.querySelector("#cityName").value;
+        let baseUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${x},${y}&aqi=no`;
 
-      try {
-        let response = await fetch(baseUrl);
-        if (!response.ok) {
-          throw new Error(
-            `Network response was not ok: ${response.statusText}`
-          );
+        try {
+          let response = await fetch(baseUrl);
+          if (!response.ok) {
+            throw new Error(
+              `Network response was not ok: ${response.statusText}`
+            );
+          }
+          let name = document.querySelector("#cityNameDisplay");
+          let temp = document.querySelector("#temperatureDisplay");
+          let humidity = document.querySelector("#humidityDisplay");
+          let weatherIcon = document.querySelector("#weatherIcon");
+          let windDisplay = document.querySelector("#windDisplay");
+          let resultJson = await response.json();
+          console.log(resultJson);
+          name.innerText = resultJson.location.name;
+          temp.innerText = `${resultJson.current.temp_c}°C`;
+          humidity.innerText = `${resultJson.current.humidity}%`;
+          weatherIcon.src = `https:${resultJson.current.condition.icon}`;
+          windDisplay.innerText = `${resultJson.current.wind_kph}kph`;
+        } catch (error) {
+          console.error("Error:", error);
         }
-        let name = document.querySelector("#cityNameDisplay");
-        let temp = document.querySelector("#temperatureDisplay");
-        let humidity = document.querySelector("#humidityDisplay");
-        let weatherIcon = document.querySelector("#weatherIcon");
-        let windDisplay = document.querySelector("#windDisplay");
-        let resultJson = await response.json();
-        console.log(resultJson);
-        name.innerText = resultJson.location.name;
-        temp.innerText = `${resultJson.current.temp_c}°C`;
-        humidity.innerText = `${resultJson.current.humidity}%`;
-        weatherIcon.src = `https:${resultJson.current.condition.icon}`;
-        windDisplay.innerText = `${resultJson.current.wind_kph}kph`;
-      } catch (error) {
-        console.error("Error:", error);
       }
     }
-  }
 
-  navigator.geolocation.getCurrentPosition((position) => {
-    weatherUpdate(position.coords.latitude, position.coords.longitude);
+    navigator.geolocation.getCurrentPosition((position) => {
+      weatherUpdate(position.coords.latitude, position.coords.longitude);
+    });
   });
 });
